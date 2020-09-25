cc3a77cdea693afa4e4b3d4616a20fa5f3cd99ce
randel <randelfreitas@gmail.com>
2020-09-25 10:46:17 -0300

    Updates

---
+++ precache-manifest.f1dc14d4261d9fd19063399a6dc5bc2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cc62e2c5a2c40802d0fe0e154059f4",
+    "url": "/teste/index.html"
+  },
+  {
+    "revision": "abc045f688d4304f6305",
+    "url": "/teste/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89a8bca63db5abf4aa71",
+    "url": "/teste/static/js/2.43d1d294.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teste/static/js/2.43d1d294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc045f688d4304f6305",
+    "url": "/teste/static/js/main.26fde2e9.chunk.js"
+  },
+  {
+    "revision": "8d0f22d780f8370be823",
+    "url": "/teste/static/js/runtime-main.96598f05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teste/static/media/logo.5d5d9eef.svg"
+  }
+]);
